2aa406573c3833bf3f251a27f7dd0145a24e5ad2
David Anthony Le <bebaoga123@gmail.com>
2022-06-11 16:57:22 +0700

    game tic tac toe

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -21,6 +21,8 @@ namespace Tic_Tac_Toe
         {
             do 
             {
+                Console.Clear();
+
                 Console.WriteLine("Tic Tac Toe Ver 0.0");
 
                 Console.WriteLine("Player1({0}) vs Player2({1})", playerChar1, playerChar2);
@@ -32,6 +34,42 @@ namespace Tic_Tac_Toe
                 pos = Convert.ToInt32(Console.ReadLine());
                 
                 checkPosition();
+
+                int flag = checkWin();
+
+                if (flag == 1)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Tic Tac Toe Ver 0.0");
+
+                    Console.WriteLine("Player1({0}) vs Player2({1})", playerChar1, playerChar2);
+
+                    Board();
+
+                    Console.Write("Player {0} WINNN!!!, Nhan phim bat ky de choi lai: ", playerWin());
+
+                    Console.ReadLine();
+
+                    resetGame();
+                }
+                else if (flag == -1)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Tic Tac Toe Ver 0.0");
+
+                    Console.WriteLine("Player1({0}) vs Player2({1})", playerChar1, playerChar2);
+
+                    Board();
+
+                    Console.Write("DRAWW !!!, Nhan phim bat ky de choi lai: ", playerWin());
+
+                    Console.ReadLine();
+
+                    resetGame();
+                }
+
             }while (true);
             
             
@@ -170,7 +208,54 @@ namespace Tic_Tac_Toe
             
 //             return ;
         }
+
+        private static char playerWin()
+        {
+            player = player % 2 == 0 ? 1 : 2;
+
+            return getChar();
+        }
+
+        private static void resetGame()
+        {
+            for (int i = 1; i < 9; i++)
+                board[i] = '-';
+
+            player = 1;
+        }
         
+        private static int checkWin()
+        {
+            int i;
+            for ( i = 1; i <= 9; i++)
+            {
+                if (board[i] == '-')
+                    break;
+                
+            }
+            if (board[1] == board[2] && board[2] == board[3] && board[2] != '-')
+                return 1;
+            else if (board[4] == board[5] && board[5] == board[6] && board[4] != '-')
+                return 1;
+            else if (board[7] == board[8] && board[8] == board[9] && board[7] != '-')
+                return 1;
+            else if (board[1] == board[4] && board[4] == board[7] && board[4] != '-')
+                return 1;
+            else if (board[2] == board[5] && board[5] == board[8] && board[5] != '-')
+                return 1;
+            else if (board[3] == board[6] && board[6] == board[9] && board[6] != '-')
+                return 1;
+            else if (board[1] == board[5] && board[5] == board[9] && board[5] != '-')
+                return 1;
+            else if (board[3] == board[5] && board[5] == board[7] && board[5] != '-')
+                return 1;
+
+            if (i > 9)
+                return -1;
+
+            return 0;
+        }
+
         private static char getChar()
         {
            return player % 2 == 0 ? playerChar1 : playerChar2;
